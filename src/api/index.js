9b38b984de6175e362db459eb580acbe8eaebaf9
AmolShelke2 <shelkeamol897@gmail.com>
2026-05-01 13:42:43 +0530

    changed api base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+// const API_BASE_URL =
+//   import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+function getApiBaseUrl() {
+  if (import.meta.env.DEV) {
+    return "http://localhost:5000/api";
+  } else {
+    return "food-management-system-server-eight.vercel.app/api";
+  }
+}
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: getApiBaseUrl(),
   headers: {
     "Content-Type": "application/json",
   },
