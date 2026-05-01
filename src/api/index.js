64e0c6d664b581bc7e0d6782ed8fab05d38df1b1
AmolShelke2 <shelkeamol897@gmail.com>
2026-05-01 13:56:33 +0530

    update api base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-// const API_BASE_URL =
-//   import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
-function getApiBaseUrl() {
+const getApiBaseUrl = () => {
   if (import.meta.env.DEV) {
     return "http://localhost:5000/api";
   } else {
-    return "food-management-system-server-eight.vercel.app/api";
+    return import.meta.env.VITE_API_URL || "http://localhost:5000/api";
   }
-}
+};
 
 const api = axios.create({
-  baseURL: getApiBaseUrl(),
+  baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
